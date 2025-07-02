032a6d854e41078eb903a94b5d837b7165e60ebf
hanminjae <hanminjae131@gmail.com>
2025-07-03 03:25:26 +0900

    [FEAT/#11] 네트워크 이미지 래퍼 컴포넌트 구현

---
+++ core/designsystem/src/main/java/com/hilingual/core/designsystem/component/image/NetworkImage.kt
@@ -0,0 +1,61 @@
+package com.hilingual.core.designsystem.component.image
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.hilingual.core.designsystem.R
+import com.hilingual.core.designsystem.theme.HilingualTheme
+
+@Composable
+fun NetworkImage(
+    imageUrl: String,
+    modifier: Modifier = Modifier,
+    shape: Shape = CircleShape,
+    contentScale: ContentScale = ContentScale.Crop,
+    contentDescription: String? = null
+) {
+    if (LocalInspectionMode.current) {
+        Image(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_default_image),
+            contentDescription = contentDescription,
+            contentScale = contentScale,
+            modifier = modifier.clip(shape)
+
+        )
+    } else {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(imageUrl)
+                .crossfade(true)
+                .build(),
+            contentDescription = contentDescription,
+            contentScale = contentScale,
+            modifier = modifier.clip(shape)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun NetworkImagePreview() {
+    HilingualTheme {
+        NetworkImage(
+            imageUrl = "",
+            modifier = Modifier.size(50.dp),
+        )
+    }
+}
