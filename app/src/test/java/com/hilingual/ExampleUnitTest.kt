4e034be0e5868b09147e5ff849b03c272944d23d
hanminjae <hanminjae131@gmail.com>
2025-07-02 16:30:31 +0900

    [INIT/#1] initial commit

--- app/src/test/java/com/hilingual/ExampleUnitTest.kt
+++ app/src/test/java/com/hilingual/ExampleUnitTest.kt
@@ -1,6 +1,6 @@
 package com.hilingual
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 /**
--- presentation/diarywrite/src/test/java/com/hilingual/presentation/diarywrite/ExampleUnitTest.kt
+++ presentation/diarywrite/src/test/java/com/hilingual/presentation/diarywrite/ExampleUnitTest.kt
@@ -2,7 +2,7 @@ package com.hilingual.presentation.diarywrite
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
