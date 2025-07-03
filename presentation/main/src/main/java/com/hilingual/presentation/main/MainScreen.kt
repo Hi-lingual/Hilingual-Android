dd47922bb879787f81da516555f1044a0125d5bc
Nahyun Kim <hedghog1004@naver.com>
2025-07-04 04:56:26 +0900

    [FEAT/#9] MainScreen BackHandler 로직 추가

--- presentation/main/src/main/java/com/hilingual/presentation/main/MainScreen.kt
+++ presentation/main/src/main/java/com/hilingual/presentation/main/MainScreen.kt
@@ -1,20 +1,44 @@
 package com.hilingual.presentation.main
 
+import android.app.Activity
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import com.hilingual.presentation.home.navigation.homeNavGraph
 import com.hilingual.presentation.main.component.MainBottomBar
 import com.hilingual.presentation.voca.navigation.vocaNavGraph
 import kotlinx.collections.immutable.toPersistentList
 
+private const val EXIT_MILLIS = 3000L
+
 @Composable
 fun MainScreen(
     navigator: MainNavigator = rememberMainNavigator()
 ) {
+    val context = LocalContext.current
+
+    var backPressedTime by remember {
+        mutableLongStateOf(0L)
+    }
+
+    BackHandler {
+        if (System.currentTimeMillis() - backPressedTime <= EXIT_MILLIS) {
+            (context as Activity).finish()
+        } else {
+            // TODO: 앱 종료 전 토스트 or 스낵바 표시
+        }
+        backPressedTime = System.currentTimeMillis()
+    }
+
     Scaffold(
         modifier = Modifier.navigationBarsPadding(),
         bottomBar = {
