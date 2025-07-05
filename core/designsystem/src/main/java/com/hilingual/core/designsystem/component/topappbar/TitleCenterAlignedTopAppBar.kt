d3c5f53a239fa4b5ab4831e62dee23261574bd98
Daljyeong <jiyoungm1022@gmail.com>
2025-07-06 06:13:01 +0900

    [FEAT/#16] TitleCenterAlignedTopAppBar 구현

---
+++ core/designsystem/src/main/java/com/hilingual/core/designsystem/component/topappbar/TitleCenterAlignedTopAppBar.kt
@@ -0,0 +1,24 @@
+package com.hilingual.core.designsystem.component.topappbar
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.hilingual.core.designsystem.theme.HilingualTheme
+
+@Composable
+fun TitleCenterAlignedTopAppBar(
+    title: String
+) {
+    HilingualBasicTopAppBar(
+        title = title
+    )
+}
+
+@Preview
+@Composable
+private fun TitleCenterAlignedTopAppBarPreview() {
+    HilingualTheme {
+        TitleCenterAlignedTopAppBar(
+            title = "일기 작성하기"
+        )
+    }
+}
