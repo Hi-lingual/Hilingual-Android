0f73c71b6a338563c3c764c909d7752af11f1fc2
Kim Nahyeon <nhyeonii@naver.com>
2025-07-05 12:40:37 +0900

    [FEAT/#15] Surface 제거

--- core/designsystem/src/main/java/com/hilingual/core/designsystem/component/button/HilingualButton.kt
+++ core/designsystem/src/main/java/com/hilingual/core/designsystem/component/button/HilingualButton.kt
@@ -1,11 +1,11 @@
 package com.hilingual.core.designsystem.component.button
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hilingual.core.common.extension.noRippleClickable
@@ -26,29 +27,26 @@ fun HilingualButton(
     modifier: Modifier = Modifier,
     enabled: Boolean = true
 ) {
-
-    Surface(
+    Row(
         modifier = modifier
             .fillMaxWidth()
+            .clip(RoundedCornerShape(8.dp))
+            .background(
+                color = if (enabled) HilingualTheme.colors.black else HilingualTheme.colors.gray300
+            )
             .noRippleClickable(
                 enabled = enabled,
                 onClick = onClick
-            ),
-        shape = RoundedCornerShape(8.dp),
-        color = if (enabled) HilingualTheme.colors.black else  HilingualTheme.colors.gray300,
-    ) {
-        Row (
-            modifier = Modifier
-                .padding(vertical = 18.dp, horizontal = 10.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = text,
-                color = HilingualTheme.colors.white,
-                style = HilingualTheme.typography.bodySB16
             )
-        }
+            .padding(vertical = 18.dp, horizontal = 10.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = text,
+            color = HilingualTheme.colors.white,
+            style = HilingualTheme.typography.bodySB16
+        )
     }
 }
 
