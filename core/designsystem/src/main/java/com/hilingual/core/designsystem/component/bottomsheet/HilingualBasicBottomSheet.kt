48fffe3858c685c70362a0fba2189099f2e38874
Nahyun Kim <hedghog1004@naver.com>
2025-07-06 23:14:59 +0900

    [MOD/#19] 매개변수로 dragHandle 옵셔널하게 받게끔 수정

--- core/designsystem/src/main/java/com/hilingual/core/designsystem/component/bottomsheet/HilingualBasicBottomSheet.kt
+++ core/designsystem/src/main/java/com/hilingual/core/designsystem/component/bottomsheet/HilingualBasicBottomSheet.kt
@@ -18,6 +18,7 @@ fun HilingualBasicBottomSheet(
     modifier: Modifier = Modifier,
     sheetState: SheetState = rememberModalBottomSheetState(),
     isDimEnabled: Boolean = true,
+    dragHandle: (@Composable () -> Unit)? = null,
     content: @Composable () -> Unit,
 ) {
     ModalBottomSheet(
@@ -27,7 +28,7 @@ fun HilingualBasicBottomSheet(
         containerColor = HilingualTheme.colors.white,
         scrimColor = if (isDimEnabled) HilingualTheme.colors.dim else Color.Transparent,
         shape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp),
-        dragHandle = null
+        dragHandle = dragHandle
     ) {
         content()
     }
