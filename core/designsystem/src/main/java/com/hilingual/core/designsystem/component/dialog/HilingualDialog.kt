73e0b8aece68e7a25564e634a1e182cddf478c2c
Kim Nahyeon <nhyeonii@naver.com>
2025-07-06 18:04:01 +0900

    [FEAT/#24] 코드 리뷰 반영

--- core/designsystem/src/main/java/com/hilingual/core/designsystem/component/dialog/HilingualDialog.kt
+++ core/designsystem/src/main/java/com/hilingual/core/designsystem/component/dialog/HilingualDialog.kt
@@ -2,12 +2,10 @@ package com.hilingual.core.designsystem.component.dialog
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,50 +15,148 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.hilingual.core.common.extension.noRippleClickable
 import com.hilingual.core.designsystem.theme.HilingualTheme
 
 @Composable
-fun HilingualDialog (
+fun HilingualDialog(
     title: String,
     description: String,
     onDismiss: () -> Unit,
     modifier: Modifier = Modifier,
+    properties: DialogProperties = DialogProperties(),
     content: @Composable () -> Unit
 ) {
     Dialog(
-        onDismissRequest = onDismiss
+        onDismissRequest = onDismiss,
+        properties = properties
     ) {
-        Box(
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp),
             modifier = modifier
                 .fillMaxWidth()
-                .wrapContentHeight()
                 .background(
                     color = HilingualTheme.colors.white,
                     shape = RoundedCornerShape(12.dp)
                 )
                 .padding(top = 34.dp, start = 24.dp, end = 24.dp, bottom = 24.dp)
         ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(10.dp),
-                modifier = modifier.fillMaxWidth()
-            ) {
-                Text(
-                    text = title,
-                    style = HilingualTheme.typography.headB16,
-                    color = HilingualTheme.colors.gray850
-                )
-                Text(
-                    text = description,
-                    style = HilingualTheme.typography.captionM12,
-                    color = HilingualTheme.colors.gray400
-                )
-
-                content()
-            }
+            Text(
+                text = title,
+                style = HilingualTheme.typography.headB16,
+                color = HilingualTheme.colors.gray850
+            )
+            Text(
+                text = description,
+                style = HilingualTheme.typography.captionM12,
+                color = HilingualTheme.colors.gray400
+            )
+            content()
         }
     }
 }
+
+@Composable
+fun OneButtonDialog(
+    title: String,
+    description: String,
+    confirmText: String,
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit,
+    modifier: Modifier = Modifier,
+    properties: DialogProperties = DialogProperties()
+) {
+    HilingualDialog(
+        title = title,
+        description = description,
+        onDismiss = onDismiss,
+        modifier = modifier,
+        properties = properties
+    ) {
+        DialogButton(
+            text = confirmText,
+            onClick = onConfirm,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun TwoButtonDialog(
+    title: String,
+    description: String,
+    cancelText: String,
+    confirmText: String,
+    onCancel: () -> Unit,
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit,
+    modifier: Modifier = Modifier,
+    properties: DialogProperties = DialogProperties()
+) {
+    HilingualDialog(
+        title = title,
+        description = description,
+        onDismiss = onDismiss,
+        modifier = modifier,
+        properties = properties
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(13.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 15.dp)
+        ) {
+            DialogButton(
+                text = cancelText,
+                onClick = onCancel,
+                isFilled = false,
+                modifier = Modifier.weight(1f)
+            )
+            DialogButton(
+                text = confirmText,
+                onClick = onConfirm,
+                isFilled = true,
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+
+}
+
+@Composable
+private fun DialogButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    isFilled: Boolean = true,
+) {
+    Row(
+        modifier = modifier
+            .clip(RoundedCornerShape(8.dp))
+            .background(
+                color = if (isFilled)
+                    HilingualTheme.colors.hilingualOrange
+                else
+                    HilingualTheme.colors.gray100
+            )
+            .padding(vertical = 14.dp)
+            .noRippleClickable(onClick = onClick),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = text,
+            style = HilingualTheme.typography.bodySB16,
+            color = if (isFilled)
+                HilingualTheme.colors.white
+            else
+                HilingualTheme.colors.gray400
+        )
+    }
+}
+
 
 @Preview
 @Composable
@@ -72,7 +168,7 @@ private fun HilingualDialogPreview() {
             onDismiss = {}
         ) {
             Row(
-                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                horizontalArrangement = Arrangement.spacedBy(13.dp),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(top = 15.dp)
@@ -110,5 +206,35 @@ private fun HilingualDialogPreview() {
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun OneButtonDialogPreview() {
+    HilingualTheme {
+        OneButtonDialog(
+            title = "회원 탈퇴하시겠어요?",
+            description = "탈퇴 시 모든 정보가 삭제되며 복구되지 않아요.",
+            confirmText = "확인",
+            onConfirm = {},
+            onDismiss = {}
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TwoButtonDialogPreview() {
+    HilingualTheme {
+        TwoButtonDialog(
+            title = "AI 피드백을 신고하시겠어요?",
+            description = "신고된 AI 피드백은 확인 후 서비스 운영원칙...",
+            cancelText = "취소",
+            confirmText = "확인",
+            onCancel = {},
+            onConfirm = {},
+            onDismiss = {}
+        )
     }
 }
