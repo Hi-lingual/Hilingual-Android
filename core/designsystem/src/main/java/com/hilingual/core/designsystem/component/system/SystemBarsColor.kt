e663a5890f43aba3e229b092212d81858461bd1e
Hyobeen-Park <amanda0109@naver.com>
2025-07-10 01:08:37 +0900

    [FEAT/#53] systemBarsColor 구현

---
+++ core/designsystem/src/main/java/com/hilingual/core/designsystem/component/system/SystemBarsColor.kt
@@ -0,0 +1,39 @@
+package com.hilingual.core.designsystem.component.system
+
+import android.os.Build
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+@Composable
+fun SystemBarsColor(
+    paddingValues: PaddingValues = PaddingValues(0.dp),
+    systemBarsColor: Color = Color.White,
+    darkIcons: Boolean = true
+) {
+    val systemUiController = rememberSystemUiController()
+
+    LaunchedEffect(Unit) {
+        systemUiController.setSystemBarsColor(
+            color = systemBarsColor,
+            darkIcons = darkIcons
+        )
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(paddingValues.calculateTopPadding())
+                .background(color = systemBarsColor)
+        )
+    }
+}
