5c9aee25353903241feb3d374703af19e61275ae
Daljyeong <jiyoungm1022@gmail.com>
2025-07-09 16:22:13 +0900

    [FEAT/#46] 텍스트 스캔하기 btn 구현

---
+++ presentation/diarywrite/src/main/java/com/hilingual/presentation/diarywrite/component/TextScanButton.kt
@@ -0,0 +1,71 @@
+package com.hilingual.presentation.diarywrite.component
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hilingual.core.common.extension.noRippleClickable
+import com.hilingual.core.designsystem.theme.HilingualTheme
+import com.hilingual.presentation.diarywrite.R
+
+@Composable
+internal fun TextScanButton(
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .clip(shape = RoundedCornerShape(8.dp))
+            .background(HilingualTheme.colors.white)
+            .border(
+                width = 1.dp,
+                color = HilingualTheme.colors.gray200,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .noRippleClickable { onClick() }
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            modifier = Modifier.size(16.dp),
+            imageVector = ImageVector.vectorResource(R.drawable.ic_scan_16),
+            contentDescription = null,
+            tint = HilingualTheme.colors.gray500
+        )
+        Text(
+            text = "텍스트 스캔하기",
+            color = HilingualTheme.colors.gray500,
+            style = HilingualTheme.typography.captionR14
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun TextScanButtonPreview() {
+    val context = LocalContext.current
+
+    HilingualTheme {
+        TextScanButton(
+            onClick = {
+                Toast.makeText(context, "TextScanButton Clicked", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        )
+    }
+}
