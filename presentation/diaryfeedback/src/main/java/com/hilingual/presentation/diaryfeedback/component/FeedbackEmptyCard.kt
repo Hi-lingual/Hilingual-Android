5d78b72f3f906f5037ef92abac28298d48aa2064
Nahyun Kim <hedghog1004@naver.com>
2025-07-10 00:00:22 +0900

    [FEAT/#51] 피드백 emptyView 구현

---
+++ presentation/diaryfeedback/src/main/java/com/hilingual/presentation/diaryfeedback/component/FeedbackEmptyCard.kt
@@ -0,0 +1,66 @@
+package com.hilingual.presentation.diaryfeedback.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hilingual.core.designsystem.theme.HilingualTheme
+import com.hilingual.presentation.diaryfeedback.R
+
+@Composable
+internal fun FeedbackEmptyCard(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .clip(RoundedCornerShape(8.dp))
+            .background(HilingualTheme.colors.white)
+            .padding(12.dp)
+    ) {
+        Image(
+            painterResource(R.drawable.img_feedback_wow),
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .size(width = 200.dp, height = 140.dp)
+        )
+
+        Spacer(Modifier.height(10.dp))
+
+        HorizontalDivider(
+            thickness = (0.5).dp,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(Modifier.height(8.dp))
+
+        Text(
+            text = "완벽한 일기네요. 틀린 부분 하나 없이 잘 썼어요!",
+            style = HilingualTheme.typography.bodyM14,
+            color = HilingualTheme.colors.hilingualBlack,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0x000000)
+@Composable
+private fun FeedbackCardPreview() {
+    HilingualTheme {
+        FeedbackEmptyCard()
+    }
+}
