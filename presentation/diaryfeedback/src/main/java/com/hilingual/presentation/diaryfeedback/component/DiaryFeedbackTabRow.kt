41d358007e513dd8358d11e14de5c7288906f6ca
Nahyun Kim <hedghog1004@naver.com>
2025-07-09 16:46:15 +0900

    [MOD/#42] TabRow 리플 효과 삭제

--- presentation/diaryfeedback/src/main/java/com/hilingual/presentation/diaryfeedback/component/DiaryFeedbackTabRow.kt
+++ presentation/diaryfeedback/src/main/java/com/hilingual/presentation/diaryfeedback/component/DiaryFeedbackTabRow.kt
@@ -1,17 +1,21 @@
 package com.hilingual.presentation.diaryfeedback.component
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalRippleConfiguration
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.hilingual.core.designsystem.theme.HilingualTheme
 import kotlinx.collections.immutable.persistentListOf
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun DiaryFeedbackTabRow(
     tabIndex: Int,
@@ -20,33 +24,35 @@ internal fun DiaryFeedbackTabRow(
 ) {
     val titles = persistentListOf("문법·철자", "추천표현")
 
-    TabRow(
-        selectedTabIndex = tabIndex,
-        containerColor = HilingualTheme.colors.white,
-        contentColor = HilingualTheme.colors.black,
-        indicator = { tabPositions ->
-            TabRowDefaults.SecondaryIndicator(
-                modifier = Modifier.tabIndicatorOffset(tabPositions[tabIndex]),
-                color = HilingualTheme.colors.black
-            )
-        },
-        divider = {},
-        modifier = modifier.padding(horizontal = 16.dp)
-    ) {
-        titles.forEachIndexed { index, title ->
-            val selected = ( tabIndex == index )
-            Tab(
-                text = {
-                    Text(
-                        text = title,
-                        style = if (selected) HilingualTheme.typography.headB18 else HilingualTheme.typography.headM18
-                    )
-                },
-                selected = selected,
-                onClick = { onTabSelected(index) },
-                selectedContentColor = HilingualTheme.colors.black,
-                unselectedContentColor = HilingualTheme.colors.gray500,
-            )
+    CompositionLocalProvider(LocalRippleConfiguration provides null) {
+        TabRow(
+            selectedTabIndex = tabIndex,
+            containerColor = HilingualTheme.colors.white,
+            contentColor = HilingualTheme.colors.black,
+            indicator = { tabPositions ->
+                TabRowDefaults.SecondaryIndicator(
+                    modifier = Modifier.tabIndicatorOffset(tabPositions[tabIndex]),
+                    color = HilingualTheme.colors.black
+                )
+            },
+            divider = {},
+            modifier = modifier.padding(horizontal = 16.dp)
+        ) {
+            titles.forEachIndexed { index, title ->
+                val selected = (tabIndex == index)
+                Tab(
+                    text = {
+                        Text(
+                            text = title,
+                            style = if (selected) HilingualTheme.typography.headB18 else HilingualTheme.typography.headM18
+                        )
+                    },
+                    selected = selected,
+                    onClick = { onTabSelected(index) },
+                    selectedContentColor = HilingualTheme.colors.black,
+                    unselectedContentColor = HilingualTheme.colors.gray500,
+                )
+            }
         }
     }
 }
