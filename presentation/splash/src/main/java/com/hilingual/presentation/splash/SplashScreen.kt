d554a912a8aa4eadaaf5c71678bab9025df85040
Hyobeen-Park <amanda0109@naver.com>
2025-07-10 18:53:35 +0900

    [FEAT/#64] 스플래시 UI 구현

---
+++ presentation/splash/src/main/java/com/hilingual/presentation/splash/SplashScreen.kt
@@ -0,0 +1,57 @@
+package com.hilingual.presentation.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import com.hilingual.core.designsystem.theme.HilingualTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@Composable
+internal fun SplashRoute(
+    navigateToAuth: () -> Unit,
+    navigateToHome: () -> Unit
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    LaunchedEffect(Unit) {
+        lifecycleOwner.lifecycleScope.launch {
+            delay(1000)
+            navigateToAuth()
+        }
+    }
+
+    SplashScreen()
+}
+
+@Composable
+private fun SplashScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(HilingualTheme.colors.hilingualOrange)
+    ) {
+        Spacer(
+            modifier = Modifier.weight(252f)
+        )
+
+        Image(
+            painter = painterResource(R.drawable.img_logo),
+            contentDescription = null
+        )
+
+        Spacer(
+            modifier = Modifier.weight(406f)
+        )
+    }
+}
