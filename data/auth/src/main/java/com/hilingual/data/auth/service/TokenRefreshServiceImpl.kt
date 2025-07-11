777d567da6f8d17553b9e70982280cf4c839f8de
hanminjae <hanminjae131@gmail.com>
2025-07-12 07:22:46 +0900

    [MOD/#60] use const value

--- data/auth/src/main/java/com/hilingual/data/auth/service/TokenRefreshServiceImpl.kt
+++ data/auth/src/main/java/com/hilingual/data/auth/service/TokenRefreshServiceImpl.kt
@@ -2,6 +2,7 @@ package com.hilingual.data.auth.service
 
 import com.hilingual.core.common.util.suspendRunCatching
 import com.hilingual.core.localstorage.TokenManager
+import com.hilingual.core.network.BEARER
 import com.hilingual.core.network.service.TokenRefreshService
 import javax.inject.Inject
 
@@ -10,7 +11,7 @@ class TokenRefreshServiceImpl @Inject constructor(
     private val tokenManager: TokenManager
 ) : TokenRefreshService {
     override suspend fun refreshToken(refreshToken: String): Result<Pair<String, String>> = suspendRunCatching {
-        val response = reissueService.reissueToken("Bearer $refreshToken")
+        val response = reissueService.reissueToken("$BEARER $refreshToken")
         val data = response.data!!
         tokenManager.saveTokens(data.accessToken, data.refreshToken)
         Pair(data.accessToken, data.refreshToken)
