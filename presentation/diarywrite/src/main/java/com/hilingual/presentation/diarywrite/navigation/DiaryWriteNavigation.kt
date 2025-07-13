b0ea40d51e249f9639c688b62f6682890fb0a579
Daljyeong <jiyoungm1022@gmail.com>
2025-07-14 05:29:55 +0900

    [FEAT/#87] DiaryWriteNavigation 생성

---
+++ presentation/diarywrite/src/main/java/com/hilingual/presentation/diarywrite/navigation/DiaryWriteNavigation.kt
@@ -0,0 +1,32 @@
+package com.hilingual.presentation.diarywrite.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.hilingual.core.navigation.Route
+import com.hilingual.presentation.diarywrite.DiaryWriteRoute
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object DiaryWrite : Route
+
+fun NavController.navigateToDiaryWrite(
+    navOptions: NavOptions? = null
+) {
+    navigate(
+        route = DiaryWrite,
+        navOptions = navOptions
+    )
+}
+
+fun NavGraphBuilder.diaryWriteNavGraph(
+    paddingValues: PaddingValues
+) {
+    composable<DiaryWrite> {
+        DiaryWriteRoute(
+            paddingValues = paddingValues
+        )
+    }
+}
