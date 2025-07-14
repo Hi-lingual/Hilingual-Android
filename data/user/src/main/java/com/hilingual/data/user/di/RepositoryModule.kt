a7a990749ba0aa95b9b8e0bf74ffe73130774e96
hanminjae <hanminjae131@gmail.com>
2025-07-14 22:41:06 +0900

    [DEL/#95] delete written time

--- data/user/src/main/java/com/hilingual/data/user/di/RepositoryModule.kt
+++ data/user/src/main/java/com/hilingual/data/user/di/RepositoryModule.kt
@@ -1,21 +1,20 @@
 package com.hilingual.data.user.di
 
-import com.hilingual.data.user.datasource.UserRemoteDataSource
 import com.hilingual.data.user.repository.UserRepository
 import com.hilingual.data.user.repositoryimpl.UserRepositoryImpl
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
+abstract class RepositoryModule {
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideUserRepository(
-        userRemoteDataSource: UserRemoteDataSource
-    ): UserRepository = UserRepositoryImpl(userRemoteDataSource)
+    abstract fun bindsUserRepository(
+        userRepositoryImpl: UserRepositoryImpl
+    ): UserRepository
 }
