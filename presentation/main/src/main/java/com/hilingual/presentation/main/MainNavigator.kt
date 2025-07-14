fcce92a70bf0a69881ce1ccd882f6d721ee849de
hanminjae <hanminjae131@gmail.com>
2025-07-15 01:35:29 +0900

    [MOD/#65] lint 수정

--- presentation/main/src/main/java/com/hilingual/presentation/main/MainNavigator.kt
+++ presentation/main/src/main/java/com/hilingual/presentation/main/MainNavigator.kt
@@ -11,7 +11,6 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navOptions
 import com.hilingual.presentation.auth.navigation.navigateToAuth
 import com.hilingual.presentation.diaryfeedback.navigation.navigateToDiaryFeedback
-import com.hilingual.presentation.home.navigation.Home
 import com.hilingual.presentation.home.navigation.navigateToHome
 import com.hilingual.presentation.onboarding.navigation.navigateToOnboarding
 import com.hilingual.presentation.splash.navigation.Splash
