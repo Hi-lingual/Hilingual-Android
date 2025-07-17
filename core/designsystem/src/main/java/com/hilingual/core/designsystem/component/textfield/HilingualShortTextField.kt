9d5b68fc9095c82cddf365e0f11d7980f012e451
hanminjae <hanminjae131@gmail.com>
2025-07-18 00:02:58 +0900

    [MOD/#130] 린트 수정

--- core/designsystem/src/main/java/com/hilingual/core/designsystem/component/textfield/HilingualShortTextField.kt
+++ core/designsystem/src/main/java/com/hilingual/core/designsystem/component/textfield/HilingualShortTextField.kt
@@ -8,8 +8,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
