5f51a2c9a001631489f81e2f85f439ac8571b598
Daljyeong <jiyoungm1022@gmail.com>
2025-07-18 16:59:09 +0900

    [FIX/#141] HilingualLongTextField 진입 시 영문 키보드가 먼저 뜨도록 설정

--- core/designsystem/src/main/java/com/hilingual/core/designsystem/component/textfield/HilingualLongTextField.kt
+++ core/designsystem/src/main/java/com/hilingual/core/designsystem/component/textfield/HilingualLongTextField.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hilingual.core.common.util.EmojiFilter.removeEmoji
@@ -48,7 +49,8 @@ fun HilingualLongTextField(
             keyboardController?.hide()
         },
         isShowLength = true,
-        keyboardImeAction = ImeAction.Default
+        keyboardImeAction = ImeAction.Default,
+        keyboardType = KeyboardType.Password
     )
 }
 
