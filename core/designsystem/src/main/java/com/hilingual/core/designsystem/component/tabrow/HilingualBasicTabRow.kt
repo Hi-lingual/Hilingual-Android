b50f9c054a9ec5637cd7b7ee95bdcce59c977ac5
Nahyun Kim <hedghog1004@naver.com>
2025-08-06 22:59:32 +0900

    [MOD/#193] TabRow 공통 컴포넌트로 이동

--- core/designsystem/src/main/java/com/hilingual/core/designsystem/component/tabrow/HilingualBasicTabRow.kt
+++ core/designsystem/src/main/java/com/hilingual/core/designsystem/component/tabrow/HilingualBasicTabRow.kt
@@ -1,19 +1,4 @@
-/*
- * Copyright 2025 The Hilingual Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.hilingual.presentation.diaryfeedback.component
+package com.hilingual.core.designsystem.component.tabrow
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -29,17 +14,17 @@ import androidx.compose.runtime.key
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.hilingual.core.designsystem.theme.HilingualTheme
-import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.PersistentList
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-internal fun DiaryFeedbackTabRow(
+fun HilingualBasicTabRow(
+    tabTitles: PersistentList<String>,
     tabIndex: Int,
     onTabSelected: (Int) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val titles = persistentListOf("문법·철자", "추천표현")
-
     CompositionLocalProvider(LocalRippleConfiguration provides null) {
         TabRow(
             selectedTabIndex = tabIndex,
@@ -54,7 +39,7 @@ internal fun DiaryFeedbackTabRow(
             divider = {},
             modifier = modifier.padding(horizontal = 16.dp)
         ) {
-            titles.forEachIndexed { index, title ->
+            tabTitles.forEachIndexed { index, title ->
                 val selected = (tabIndex == index)
                 key(index, title) {
                     Tab(
