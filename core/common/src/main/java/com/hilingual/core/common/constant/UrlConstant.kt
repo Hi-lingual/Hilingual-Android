a8b5555a66c28b2c718432d26a0f9435e401924e
hanminjae <hanminjae131@gmail.com>
2025-08-12 00:34:32 +0900

    [MOD/#215] Url 중앙화

---
+++ core/common/src/main/java/com/hilingual/core/common/constant/UrlConstant.kt
@@ -0,0 +1,8 @@
+package com.hilingual.core.common.constant
+
+object UrlConstant {
+    const val PRIVACY_POLICY = "https://hilingual.notion.site/017fad44183a47f68b95c27181595b40"
+    const val SERVICE_TERMS = "https://hilingual.notion.site/017fad44183a47f68b95c27181595b40"
+    const val ONBOARDING_PRIVACY_POLICY = "https://hilingual.notion.site/017fad44183a47f68b95c27181595b40"
+    const val FEEDBACK_REPORT = "https://hilingual.notion.site/230829677ebf801c965be24b0ef444e9"
+}
