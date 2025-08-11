b2995aaa0f579b75d3aee8c5e974a3bae9d4d28f
Nahyun Kim <hedghog1004@naver.com>
2025-08-12 00:33:02 +0900

    [FIX/#193] 줄바꿈 수정

--- core/designsystem/src/main/java/com/hilingual/core/designsystem/component/toggle/HilingualBasicToggleSwitch.kt
+++ core/designsystem/src/main/java/com/hilingual/core/designsystem/component/toggle/HilingualBasicToggleSwitch.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.hilingual.core.designsystem.theme.HilingualTheme
+
 @Composable
 fun HilingualBasicToggleSwitch(
     isChecked: Boolean,
