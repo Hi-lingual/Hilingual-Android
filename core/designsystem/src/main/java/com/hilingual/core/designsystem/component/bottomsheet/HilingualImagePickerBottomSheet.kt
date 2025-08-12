924453c17c77a72309ce8e8f941b4dc04967683f
hanminjae <hanminjae131@gmail.com>
2025-08-12 23:29:40 +0900

    [FEAT/#232] 프로필 이미지 선택 바텀시트 구현

---
+++ core/designsystem/src/main/java/com/hilingual/core/designsystem/component/bottomsheet/HilingualImagePickerBottomSheet.kt
@@ -0,0 +1,49 @@
+package com.hilingual.core.designsystem.component.bottomsheet
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hilingual.core.designsystem.R
+import com.hilingual.core.designsystem.theme.HilingualTheme
+
+@Composable
+fun HilingualProfileImageBottomSheet(
+    isVisible: Boolean,
+    onDismiss: () -> Unit,
+    onDefaultImageClick: () -> Unit,
+    onGalleryImageClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    HilingualMenuBottomSheet(
+        isVisible = isVisible,
+        onDismiss = onDismiss,
+        title = "이미지 선택",
+        modifier = modifier
+    ) {
+        HilingualMenuBottomSheetItem(
+            text = "기본 이미지로 변경하기",
+            iconResId = R.drawable.ic_image_24,
+            onClick = onDefaultImageClick
+        )
+
+        HilingualMenuBottomSheetItem(
+            text = "갤러리에서 선택하기",
+            iconResId = R.drawable.ic_gallary_24,
+            onClick = onGalleryImageClick
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun HilingualImagePickerBottomSheetPreview() {
+    HilingualTheme{
+        HilingualProfileImageBottomSheet(
+            isVisible = true,
+            onDismiss = {},
+            onDefaultImageClick = {},
+            onGalleryImageClick = {}
+        )
+    }
+}
+
