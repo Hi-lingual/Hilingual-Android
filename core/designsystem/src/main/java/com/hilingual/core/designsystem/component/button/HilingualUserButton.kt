cd3d2395fa20c5a72f6ec1766ddf1edce3753023
Kim Nahyeon <nhyeonii@naver.com>
2025-08-13 02:30:05 +0900

    [FEAT/#228] 유저 아이템 구현 추가

--- core/designsystem/src/main/java/com/hilingual/core/designsystem/component/button/HilingualUserButton.kt
+++ core/designsystem/src/main/java/com/hilingual/core/designsystem/component/button/HilingualUserButton.kt
@@ -3,19 +3,29 @@ package com.hilingual.core.designsystem.component.button
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.hilingual.core.common.extension.noRippleClickable
+import com.hilingual.core.designsystem.component.image.NetworkImage
+import com.hilingual.core.designsystem.theme.HilingualTheme
 import com.hilingual.core.designsystem.theme.gray200
 import com.hilingual.core.designsystem.theme.gray500
 import com.hilingual.core.designsystem.theme.hilingualBlack
@@ -79,7 +89,7 @@ fun FollowButton(
                     )
                 } else Modifier
             )
-            .clickable { onClick() }
+            .noRippleClickable(onClick = onClick)
             .padding(horizontal = buttonState.horizontalPadding.dp, vertical = 8.dp)
     ) {
         Text(
@@ -111,5 +121,86 @@ fun FollowButtonPreview() {
             onClick = { }
         )
 
+    }
+}
+
+@Composable
+fun UserItem(
+    profileImageUrl: String,
+    nickname: String,
+    buttonStateValue: Int,
+    onProfileClick: () -> Unit,
+    onButtonClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(HilingualTheme.colors.white)
+            .padding(vertical = 8.dp)
+            .noRippleClickable(onClick = onProfileClick),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        NetworkImage(
+            imageUrl = profileImageUrl,
+            shape = CircleShape,
+            modifier = Modifier
+                .size(42.dp)
+                .border(
+                    width = 1.dp,
+                    color = HilingualTheme.colors.gray200,
+                    shape = CircleShape
+                ),
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Text(
+            text = nickname,
+            style = HilingualTheme.typography.headB16,
+            color = HilingualTheme.colors.black,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        FollowButton(
+            buttonStateValue = buttonStateValue,
+            onClick = onButtonClick
+        )
+    }
+}
+
+@Preview()
+@Composable
+fun UserItemPreview() {
+    HilingualTheme {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            UserItem(
+                profileImageUrl = "",
+                nickname = "닉네임",
+                onProfileClick = { },
+                onButtonClick = { },
+                buttonStateValue = 1,
+            )
+            UserItem(
+                profileImageUrl = "",
+                nickname = "닉네임",
+                onProfileClick = { },
+                onButtonClick = { },
+                buttonStateValue = 2,
+            )
+            UserItem(
+                profileImageUrl = "",
+                nickname = "닉네임",
+                onProfileClick = { },
+                onButtonClick = { },
+                buttonStateValue = 3,
+            )
+        }
     }
 }
