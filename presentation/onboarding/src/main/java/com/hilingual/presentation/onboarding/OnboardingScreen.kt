7eeec233b07e9ee9067321cd5ded894d95b083ed
hanminjae <hanminjae131@gmail.com>
2025-08-13 18:43:52 +0900

    [MOD/#226] 린트 수정

--- presentation/onboarding/src/main/java/com/hilingual/presentation/onboarding/OnboardingScreen.kt
+++ presentation/onboarding/src/main/java/com/hilingual/presentation/onboarding/OnboardingScreen.kt
@@ -31,9 +31,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.tooling.preview.Preview
@@ -53,7 +50,6 @@ import com.hilingual.core.designsystem.event.LocalDialogEventProvider
 import com.hilingual.core.designsystem.theme.HilingualTheme
 import com.hilingual.core.designsystem.theme.white
 import com.hilingual.presentation.onboarding.component.TermsBottomSheet
-import com.hilingual.core.designsystem.R as DesignSystemR
 
 @Composable
 internal fun OnboardingRoute(
