280d8ef0b4fb94f056a52ef1b421dec9a45d8ef3
Nahyun Kim <hedghog1004@naver.com>
2025-08-14 04:21:08 +0900

    [REF/#224] DropdownMenu 컴포넌트 Popup 사용 방식으로 변경

--- core/designsystem/src/main/java/com/hilingual/core/designsystem/component/dropdown/HilingualBasicDropdownMenu.kt
+++ core/designsystem/src/main/java/com/hilingual/core/designsystem/component/dropdown/HilingualBasicDropdownMenu.kt
@@ -3,69 +3,90 @@ package com.hilingual.core.designsystem.component.dropdown
 import android.util.Log
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
+import com.hilingual.core.common.extension.dropShadow
 import com.hilingual.core.designsystem.R
 import com.hilingual.core.designsystem.theme.HilingualTheme
 
 @Composable
 fun HilingualBasicDropdownMenu(
-    expanded: Boolean,
-    onExpandedChange: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
-    content: @Composable ColumnScope.() -> Unit
+    offsetY: Dp = 4.dp,
+    content: @Composable () -> Unit
 ) {
-    Box(
-        modifier = modifier.padding(16.dp)
-    ) {
-        IconButton(
-            onClick = { onExpandedChange(!expanded) }
-        ) {
-            Icon(
-                Icons.Default.MoreVert,
-                contentDescription = null
-            )
-        }
+    var expanded by remember { mutableStateOf(false) }
+    var iconHeight by remember { mutableIntStateOf(0) }
+    val density = LocalDensity.current
 
-        DropdownMenu(
-            expanded = expanded,
-            offset = DpOffset(x = 0.dp, y = 4.dp),
-            shape = RoundedCornerShape(10.dp),
-            onDismissRequest = { onExpandedChange(false) },
+    val popupYOffset = with(density) { offsetY.roundToPx() }
+
+    Box {
+        Icon(
+            imageVector = Icons.Default.MoreVert,
+            contentDescription = null,
             modifier = Modifier
-                .background(HilingualTheme.colors.white)
-                .width(182.dp)
-                .padding(0.dp)
-        ) {
-            content()
+                .onGloballyPositioned { coordinates ->
+                    iconHeight = coordinates.size.height
+                }
+                .clickable(onClick = { expanded = true })
+        )
+
+        if (expanded) {
+            Popup(
+                alignment = Alignment.TopEnd,
+                onDismissRequest = { expanded = false },
+                offset = IntOffset(x = 0, y = iconHeight + popupYOffset),
+            ) {
+                Column(
+                    modifier = modifier
+                        .dropShadow(
+                            color = HilingualTheme.colors.black.copy(alpha = 0.2f),
+                            shape = RoundedCornerShape(size = 10.dp),
+                            offsetX = 0.dp,
+                            offsetY = 0.dp,
+                            spread = 0.dp,
+                            blur = 15.dp
+                        )
+                        .clip(RoundedCornerShape(size = 10.dp))
+                        .background(color = Color.White)
+                        .width(182.dp)
+                ) {
+                    content()
+                }
+            }
         }
     }
 }
@@ -77,63 +98,53 @@ fun HilingualDropdownMenuItem(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    DropdownMenuItem(
-        text = {
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .padding(12.dp)
-                    .fillMaxWidth()
-            ) {
-                Icon(
-                    imageVector = ImageVector.vectorResource(id = iconResId),
-                    contentDescription = null,
-                    modifier = Modifier.size(24.dp),
-                    tint = HilingualTheme.colors.gray400
-                )
-
-                Text(
-                    text = text,
-                    style = HilingualTheme.typography.bodySB14,
-                    color = HilingualTheme.colors.gray700
-                )
-            }
-        },
-        onClick = onClick,
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
         modifier = modifier
-    )
+            .clickable(onClick = onClick)
+            .padding(12.dp)
+            .fillMaxWidth()
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(iconResId),
+            tint = Color.Unspecified,
+            contentDescription = null
+        )
+
+        Text(
+            text = text,
+            style = HilingualTheme.typography.bodySB14,
+            color = HilingualTheme.colors.gray700
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-private fun Preview() {
+private fun DropdownMenuPreview() {
     HilingualTheme {
-        var expanded by remember { mutableStateOf(false) }
-
         Column(
             horizontalAlignment = Alignment.End,
             modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp)
         ) {
-            HilingualBasicDropdownMenu(
-                expanded = expanded,
-                onExpandedChange = { expanded = it }
-            ) {
+            HilingualBasicDropdownMenu {
                 HilingualDropdownMenuItem(
                     text = "Option 1",
                     iconResId = R.drawable.ic_search_20,
                     onClick = {
-                        expanded = false
                         Log.d("DropdownMenu", "Option 1 clicked")
                     }
+                )
+                HorizontalDivider(
+                    thickness = 1.dp,
+                    color = HilingualTheme.colors.gray200
                 )
                 HilingualDropdownMenuItem(
                     text = "Option 2",
                     iconResId = R.drawable.ic_search_20,
                     onClick = {
-                        expanded = false
                         Log.d("DropdownMenu", "Option 2 clicked")
                     }
                 )
