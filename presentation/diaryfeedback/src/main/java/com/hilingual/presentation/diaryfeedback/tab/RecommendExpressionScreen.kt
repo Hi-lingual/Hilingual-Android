b5e3385be59096b2363a449f11924eb82ab6c35c
Nahyun Kim <hedghog1004@naver.com>
2025-08-16 00:19:21 +0900

    [FIX/#255] 탭 간 날짜 위치 이슈 수정

--- presentation/diaryfeedback/src/main/java/com/hilingual/presentation/diaryfeedback/tab/RecommendExpressionScreen.kt
+++ presentation/diaryfeedback/src/main/java/com/hilingual/presentation/diaryfeedback/tab/RecommendExpressionScreen.kt
@@ -19,12 +19,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,7 +57,10 @@ internal fun RecommendExpressionScreen(
             Text(
                 text = writtenDate,
                 style = HilingualTheme.typography.bodySB16,
-                color = HilingualTheme.colors.gray700
+                color = HilingualTheme.colors.gray700,
+                modifier = Modifier
+                    .heightIn(min = 28.dp)
+                    .wrapContentHeight(align = Alignment.CenterVertically),
             )
         }
 
