ae84ba765c32bb32d12a67d324aa8de8eda0d76f
Kim Nahyeon <nhyeonii@naver.com>
2025-08-16 01:45:18 +0900

    [FEAT/#257] FeedEmptyCard 구현

---
+++ presentation/feedprofile/src/main/java/com/hilingual/presentation/feedprofile/component/FeedEmptyCard.kt
@@ -0,0 +1,73 @@
+package com.hilingual.presentation.feedprofile.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import com.hilingual.core.designsystem.theme.HilingualTheme
+import com.hilingual.core.designsystem.R as DesignSystemR
+
+internal enum class FeedEmptyCardType(
+    val text: String
+) {
+    NOT_SHARED(
+        text = "피드에 아직 공유한 일기가 없어요."
+    ),
+    NOT_LIKED(
+        text = "아직 공감한 일기가 없어요."
+    )
+}
+
+@Composable
+internal fun FeedEmptyCard(
+    type: FeedEmptyCardType,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+
+    ) {
+        Image(
+            modifier = Modifier
+                .size(width = 200.dp, height = 100.dp),
+            painter = painterResource(id = DesignSystemR.drawable.img_diary_empty),
+            contentDescription = null
+        )
+        Text(
+            text = type.text,
+            style = HilingualTheme.typography.headM18,
+            color = HilingualTheme.colors.gray500,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+private class FeedEmptyCardPreviewParameterProvider :
+    PreviewParameterProvider<FeedEmptyCardType> {
+    override val values: Sequence<FeedEmptyCardType>
+        get() = FeedEmptyCardType.entries.asSequence()
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun FeedEmptyCardPreview(
+    @PreviewParameter(FeedEmptyCardPreviewParameterProvider::class)
+    type: FeedEmptyCardType
+) {
+    HilingualTheme {
+        FeedEmptyCard(type = type)
+    }
+}
