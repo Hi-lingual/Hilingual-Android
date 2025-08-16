959e0a7c674e9aa8fe25334f0038f862cd453372
hanminjae <hanminjae131@gmail.com>
2025-08-16 21:26:58 +0900

    [MOD/#264] 아이콘 리소스 변경

--- presentation/home/src/main/java/com/hilingual/presentation/home/component/footer/WriteDiaryButton.kt
+++ presentation/home/src/main/java/com/hilingual/presentation/home/component/footer/WriteDiaryButton.kt
@@ -36,7 +36,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hilingual.core.common.extension.noRippleClickable
 import com.hilingual.core.designsystem.theme.HilingualTheme
-import com.hilingual.presentation.home.R
+import com.hilingual.core.designsystem.R
 
 @Composable
 internal fun WriteDiaryButton(
