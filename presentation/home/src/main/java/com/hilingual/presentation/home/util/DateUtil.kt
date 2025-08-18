f7ac04f96e6735eab5fa76d3166a49546c4d1995
hanminjae <hanminjae131@gmail.com>
2025-08-18 20:33:12 +0900

    [REFACTOR/#264] home DateUtil 로직 개선

--- presentation/home/src/main/java/com/hilingual/presentation/home/util/DateUtil.kt
+++ presentation/home/src/main/java/com/hilingual/presentation/home/util/DateUtil.kt
@@ -3,13 +3,12 @@ package com.hilingual.presentation.home.util
 import com.hilingual.presentation.home.model.DateUiModel
 import java.time.LocalDate
 
-internal fun isDateWritten(date: LocalDate, dateList: List<DateUiModel>): Boolean {
-    return dateList.any { LocalDate.parse(it.date) == date }
-}
+internal fun isDateWritten(date: LocalDate, dateList: List<DateUiModel>): Boolean =
+    dateList.any {
+        runCatching { LocalDate.parse(it.date) }.getOrNull() == date
+    }
 
-internal fun isDateFuture(date: LocalDate): Boolean {
-    return date.isAfter(LocalDate.now())
-}
+internal fun isDateFuture(date: LocalDate): Boolean = date.isAfter(LocalDate.now())
 
 internal fun isDateWritable(date: LocalDate): Boolean {
     val today = LocalDate.now()
