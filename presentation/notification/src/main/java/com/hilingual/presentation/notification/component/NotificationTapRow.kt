279df9b3783da3126d1b9df1d8c6dee4c7693e00
hanminjae <hanminjae131@gmail.com>
2025-08-19 02:47:38 +0900

    [MOD/#269] Add preview

--- presentation/notification/src/main/java/com/hilingual/presentation/notification/component/NotificationTapRow.kt
+++ presentation/notification/src/main/java/com/hilingual/presentation/notification/component/NotificationTapRow.kt
@@ -1,20 +1,39 @@
 package com.hilingual.presentation.notification.component
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.hilingual.core.designsystem.component.tabrow.HilingualBasicTabRow
+import com.hilingual.core.designsystem.theme.HilingualTheme
 import kotlinx.collections.immutable.persistentListOf
 
 @Composable
 internal fun NotificationTapRow(
+    tabIndex: Int,
     onTabSelected: (Int) -> Unit,
     modifier: Modifier = Modifier
 ) {
     val titles = persistentListOf("피드", "공지사항")
     HilingualBasicTabRow(
         tabTitles = titles,
-        tabIndex = titles.size,
+        tabIndex = tabIndex,
         onTabSelected = onTabSelected,
         modifier = modifier
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+internal fun NotificationTapRowPreview() {
+    HilingualTheme {
+        var selectedTabIndex by remember { mutableIntStateOf(0) }
+        NotificationTapRow(
+            tabIndex = selectedTabIndex,
+            onTabSelected = { selectedTabIndex = it }
+        )
+    }
 }
