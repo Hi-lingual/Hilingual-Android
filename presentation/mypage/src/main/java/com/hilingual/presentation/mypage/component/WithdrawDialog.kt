b3b972dd6ca69fbe59282d1b2ca1f6a859d53a9f
Daljyeong <jiyoungm1022@gmail.com>
2025-08-21 00:39:09 +0900

    [FEAT/#267] 마이페이지 WithdrawDialog 구현

---
+++ presentation/mypage/src/main/java/com/hilingual/presentation/mypage/component/WithdrawDialog.kt
@@ -0,0 +1,36 @@
+package com.hilingual.presentation.mypage.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.hilingual.core.designsystem.component.dialog.TwoButtonDialog
+import com.hilingual.core.designsystem.theme.HilingualTheme
+
+@Composable
+internal fun WithdrawDialog(
+    onDismiss: () -> Unit,
+    onNoClick: () -> Unit,
+    onCancelClick: () -> Unit
+) {
+    TwoButtonDialog(
+        title = "정말 계정을 삭제하시겠어요?",
+        description = "회원 탈퇴 시 작성한 일기를 비롯한 계정 정보는 \n" +
+            "영원히 삭제돼요. 정말 삭제를 원하시나요?",
+        cancelText = "아니요",
+        confirmText = "삭제하기",
+        onNegative = onNoClick,
+        onPositive = onCancelClick,
+        onDismiss = onDismiss
+    )
+}
+
+@Preview
+@Composable
+private fun WithdrawDialogPreview() {
+    HilingualTheme {
+        WithdrawDialog(
+            onDismiss = {},
+            onNoClick = {},
+            onCancelClick = {}
+        )
+    }
+}
