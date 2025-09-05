a8e84512f75d4ea5d25601a45501282b92ae0c93
Kim Nahyeon <nhyeonii@naver.com>
2025-09-05 16:10:11 +0900

    [REF/#342] 피드 프로필 팔로우 관련 로직 분리

--- presentation/feedprofile/src/main/java/com/hilingual/presentation/feedprofile/follow/model/FollowItemModel.kt
+++ presentation/feedprofile/src/main/java/com/hilingual/presentation/feedprofile/follow/model/FollowItemModel.kt
@@ -1,7 +1,7 @@
 package com.hilingual.presentation.feedprofile.follow.model
 
 import androidx.compose.runtime.Immutable
-import com.hilingual.data.feed.model.FollowState
+import com.hilingual.data.user.model.FollowState
 
 @Immutable
 data class FollowItemModel(
