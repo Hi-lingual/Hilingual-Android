4ab85b12092adc32c345190bdcd5bdd70a0bec3a
Nahyun Kim <hedghog1004@naver.com>
2025-09-06 14:03:21 +0900

    [DEL/#343] 주석 코드 삭제

--- data/diary/src/main/java/com/hilingual/data/diary/model/BookmarkResult.kt
+++ data/diary/src/main/java/com/hilingual/data/diary/model/BookmarkResult.kt
@@ -1,10 +1,5 @@
 package com.hilingual.data.diary.model
 
-// sealed interface BookmarkResult {
-//    data object isSuccess : BookmarkResult
-//    data class isError(val errorType: BookmarkErrorCode) : BookmarkResult
-// }
-
 enum class BookmarkResult(val code: Int) {
     DEFAULT(-1),
     SUCCESS(20000),
