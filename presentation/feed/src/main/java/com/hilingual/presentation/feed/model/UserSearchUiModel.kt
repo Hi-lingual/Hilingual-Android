a5cb6fa946584413a2a4196e1baf2344a3772096
Nahyun Kim <hedghog1004@naver.com>
2025-09-09 06:22:28 +0900

    [FEAT/#359] 검색 API 연동

--- presentation/feed/src/main/java/com/hilingual/presentation/feed/model/UserSearchUiModel.kt
+++ presentation/feed/src/main/java/com/hilingual/presentation/feed/model/UserSearchUiModel.kt
@@ -1,6 +1,7 @@
 package com.hilingual.presentation.feed.model
 
 import com.hilingual.data.feed.model.FollowState
+import com.hilingual.data.feed.model.UserListModel
 
 internal data class UserSearchUiModel(
     val userId: Long,
@@ -8,3 +9,14 @@ internal data class UserSearchUiModel(
     val profileUrl: String,
     val followState: FollowState
 )
+
+internal fun UserListModel.toState(): List<UserSearchUiModel> {
+    return this.userList.map {
+        UserSearchUiModel(
+            userId = it.userId,
+            nickname = it.nickname,
+            profileUrl = it.profileImg,
+            followState = it.followState
+        )
+    }
+}
