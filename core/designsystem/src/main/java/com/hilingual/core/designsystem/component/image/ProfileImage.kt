019fd92056d9011262c67abe1b78aa2acd0ac48d
Nahyun Kim <hedghog1004@naver.com>
2025-09-09 03:21:12 +0900

    [ADD/#368] ProfileImage 공통 컴포넌트 구현

---
+++ core/designsystem/src/main/java/com/hilingual/core/designsystem/component/image/ProfileImage.kt
@@ -0,0 +1,33 @@
+package com.hilingual.core.designsystem.component.image
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import com.hilingual.core.designsystem.R
+
+@Composable
+fun ProfileImage(
+    imageUrl: String?,
+    size: Dp,
+    modifier: Modifier = Modifier,
+) {
+    if (imageUrl.isNullOrBlank()) {
+        Image(
+            painter = painterResource(R.drawable.img_default_image),
+            contentDescription = null,
+            modifier = modifier
+                .size(size)
+                .clip(CircleShape)
+        )
+    } else {
+        NetworkImage(
+            imageUrl = imageUrl,
+            modifier = modifier.size(size)
+        )
+    }
+}
