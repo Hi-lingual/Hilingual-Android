9b25c2279c70205180943415243457df00de8263
MinJae Han <hanminjae131@gmail.com>
2025-10-02 10:23:51 +0900

    [chore] androidx.test:runner 의존성 추가 (#442)

--- baselineprofile/build.gradle.kts
+++ baselineprofile/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.androidx.junit)
     implementation(libs.androidx.uiautomator)
     implementation(libs.androidx.benchmark.macro.junit4)
+    implementation(libs.androidx.runner)
 }
 
 androidComponents {
