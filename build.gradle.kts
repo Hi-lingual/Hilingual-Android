5391457d51cf29440aa7df1698b03131e960c989
hanminjae <hanminjae131@gmail.com>
2025-10-04 16:13:08 +0900

    [FEAT/#444] Firebase Crashlytics 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,8 @@ plugins {
     alias(libs.plugins.spotless) apply false
     alias(libs.plugins.android.test) apply false
     alias(libs.plugins.baselineprofile) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
 }
 
 subprojects {
