35930f1470546443fdda02c11bb6def13de99d5a
Daljyeong <jiyoungm1022@gmail.com>
2025-11-19 05:27:35 +0900

    [REF] 일기 임시 저장 로직 DataSource 계층으로 분리

--- data/diary/src/main/java/com/hilingual/data/diary/localstorage/DiaryTempRepository.kt
+++ data/diary/src/main/java/com/hilingual/data/diary/localstorage/DiaryTempRepository.kt
@@ -1,24 +1,24 @@
 package com.hilingual.data.diary.localstorage
 
 import android.net.Uri
-import com.hilingual.core.localstorage.DiaryTempManager
 import java.time.LocalDate
 import javax.inject.Inject
 
 class DiaryTempRepository @Inject constructor(
-    private val diaryTempManager: DiaryTempManager
+    private val diaryTempDataSource: DiaryTempDataSource
 ) {
     suspend fun isDiaryTempExist(selectedDate: LocalDate): Boolean =
-        diaryTempManager.isDiaryTempExist(selectedDate)
+        diaryTempDataSource.isDiaryTempExist(selectedDate)
 
     suspend fun saveDiary(selectedDate: LocalDate, text: String, imageUri: Uri?) =
-        diaryTempManager.saveDiary(selectedDate, text, imageUri)
+        diaryTempDataSource.saveDiary(selectedDate, text, imageUri)
 
     suspend fun getDiaryText(selectedDate: LocalDate): String? =
-        diaryTempManager.getDiaryText(selectedDate)
+        diaryTempDataSource.getDiaryText(selectedDate)
 
     suspend fun getDiaryImageUri(selectedDate: LocalDate): String? =
-        diaryTempManager.getDiaryImageUri(selectedDate)
+        diaryTempDataSource.getDiaryImageUri(selectedDate)
 
-    suspend fun clearDiaryTemp(selectedDate: LocalDate) = diaryTempManager.clearDiaryTemp(selectedDate)
+    suspend fun clearDiaryTemp(selectedDate: LocalDate) =
+        diaryTempDataSource.clearDiaryTemp(selectedDate)
 }
