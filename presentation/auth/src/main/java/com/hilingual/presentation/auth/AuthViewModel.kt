2ddefc6284b6e3def21f1b4aa3ff3eea35aa40f6
hanminjae <hanminjae131@gmail.com>
2025-12-10 23:39:30 +0900

    lint format

--- presentation/auth/src/main/java/com/hilingual/presentation/auth/AuthViewModel.kt
+++ presentation/auth/src/main/java/com/hilingual/presentation/auth/AuthViewModel.kt
@@ -20,7 +20,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hilingual.core.common.extension.onLogFailure
 import com.hilingual.data.auth.repository.AuthRepository
-import com.hilingual.data.user.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
