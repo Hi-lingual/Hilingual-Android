1cce7fdee062b9cf53322cee737a7bd4847acccb
hanminjae <hanminjae131@gmail.com>
2026-01-12 20:45:08 +0900

    chore: add ConfigConstants.kt with versioning constants

---
+++ data/config/src/main/java/com/hilingual/data/config/constant/ConfigConstants.kt
@@ -0,0 +1,8 @@
+package com.hilingual.data.config.constant
+
+const val MIN_FETCH_INTERVAL_SECONDS = 3600L
+
+const val KEY_MIN_FORCE_VERSION = "min_force_version"
+const val KEY_LATEST_VERSION = "latest_version"
+
+const val DEFAULT_VERSION = "0.0.0"
