458d8064dec2e9813a1ac78dc4f5982e80f072f5
hanminjae <hanminjae131@gmail.com>
2026-02-04 23:53:32 +0900

    feat: Add Firebase Cloud Messaging dependencies and configuration

---
+++ app/src/main/java/com/hilingual/service/HilingualFirebaseMessagingService.kt
@@ -0,0 +1,9 @@
+package com.hilingual.service
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HilingualFirebaseMessagingService : FirebaseMessagingService() {
+    
+}
