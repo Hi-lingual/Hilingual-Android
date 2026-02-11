5531e8a6bfb4bca3d5df87e4ed587e94ccf9dfad
Hyobeen-Park <amanda0109@naver.com>
2026-02-11 22:49:47 +0900

    chore: 코드 가독성 개선

--- core/localstorage/src/main/java/com/hilingual/core/localstorage/OnboardingStateManager.kt
+++ core/localstorage/src/main/java/com/hilingual/core/localstorage/OnboardingStateManager.kt
@@ -2,5 +2,6 @@ package com.hilingual.core.localstorage
 
 interface OnboardingStateManager {
     suspend fun getIsHomeOnboardingCompleted(): Boolean
+
     suspend fun updateIsHomeOnboardingCompleted(isCompleted: Boolean)
 }
