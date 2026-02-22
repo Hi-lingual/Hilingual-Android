ee87069bebab5956fc761f0af61f1d8118503db2
Hyobeen-Park <amanda0109@naver.com>
2026-02-22 22:41:06 +0900

    spotlessApply

--- data/config/src/main/java/com/hilingual/data/config/datasourceimpl/RemoteConfigDataSourceImpl.kt
+++ data/config/src/main/java/com/hilingual/data/config/datasourceimpl/RemoteConfigDataSourceImpl.kt
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.hilingual.data.config.datasourceimpl
 
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
