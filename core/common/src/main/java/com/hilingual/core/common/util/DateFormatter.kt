49cabd73165ca6f2d838d02bcc4b592ba878fadb
Nahyun Kim <hedghog1004@naver.com>
2026-04-27 01:42:12 +0900

    refactor: use SYSTEM_ZONE_ID for cache time zone

--- core/common/src/main/java/com/hilingual/core/common/util/DateFormatter.kt
+++ core/common/src/main/java/com/hilingual/core/common/util/DateFormatter.kt
@@ -21,6 +21,8 @@ import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
+val SYSTEM_ZONE_ID: ZoneId = ZoneId.systemDefault()
+
 object DateFormatters {
     val KOREAN_FULL_DATE: DateTimeFormatter = DateTimeFormatter.ofPattern("M월 d일 EEEE", Locale.KOREA)
 
@@ -50,14 +52,14 @@ fun String.toLocalDate(): LocalDate = LocalDate.parse(this, DateFormatters.ISO_D
  * @return "M월 d일" 형식의 문자열 (예: "4월 12일")
  */
 fun Instant.toKoreanShortDate(): String =
-    atZone(ZoneId.systemDefault()).toLocalDate().format(DateFormatters.KOREAN_SHORT_DATE)
+    atZone(SYSTEM_ZONE_ID).toLocalDate().format(DateFormatters.KOREAN_SHORT_DATE)
 
 /**
  * Instant를 점으로 구분된 전체 날짜 형식으로 변환합니다.
  * @return "yyyy.MM.dd" 형식의 문자열 (예: "2026.04.12")
  */
 fun Instant.toFullDotDate(): String =
-    atZone(ZoneId.systemDefault()).toLocalDate().format(DateFormatters.FULL_DOT_DATE)
+    atZone(SYSTEM_ZONE_ID).toLocalDate().format(DateFormatters.FULL_DOT_DATE)
 
 /**
  * LocalDate를 연도 두 자리의 점으로 구분된 날짜 형식으로 변환합니다.
