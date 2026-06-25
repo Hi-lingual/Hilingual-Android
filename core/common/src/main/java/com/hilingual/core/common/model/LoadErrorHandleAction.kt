316eeb2a30cb021bc0e5a0e62f2c74b5aec0db6b
Nahyun Kim <hedghog1004@naver.com>
2026-06-25 18:43:26 +0900

    refactor: Mark LoadErrorHandleAction as stable

--- core/common/src/main/java/com/hilingual/core/common/model/LoadErrorHandleAction.kt
+++ core/common/src/main/java/com/hilingual/core/common/model/LoadErrorHandleAction.kt
@@ -1,5 +1,8 @@
 package com.hilingual.core.common.model
 
+import androidx.compose.runtime.Stable
+
+@Stable
 sealed class LoadErrorHandleAction {
     data class Common(
         val actionType: LoadErrorActionType,
