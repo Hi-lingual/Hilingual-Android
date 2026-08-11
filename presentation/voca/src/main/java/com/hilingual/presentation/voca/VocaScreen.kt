843b183d87c07ab9d9a29d7a6fc874da0c0540d5
Nahyun Kim <hedghog1004@naver.com>
2026-08-11 16:49:31 +0900

    refactor: 단어장 북마크 이벤트 수집 제거
    단어장에서는 북마크 수집이 필요하지 않아 제거한다.
    이로써 bookmark_action은 일기 화면 두 곳에서만 동일한 스키마로 수집된다.

--- presentation/voca/src/main/java/com/hilingual/presentation/voca/VocaScreen.kt
+++ presentation/voca/src/main/java/com/hilingual/presentation/voca/VocaScreen.kt
@@ -166,15 +166,6 @@ internal fun VocaRoute(
             },
 
             onBookmarkClick = { phraseId, isMarked ->
-                tracker.logGlobalAction(
-                    trigger = TriggerType.CLICK,
-                    action = "bookmark_action",
-                    properties = mapOf(
-                        "phrase_id" to phraseId,
-                        "bookmark_action" to if (isMarked) "add" else "remove",
-                    ),
-                    currentPage = VOCABULARY,
-                )
                 viewModel.toggleBookmark(phraseId = phraseId, isMarked = isMarked)
             },
             onSearchTextChanged = viewModel::updateSearchKeyword,
@@ -196,15 +187,6 @@ internal fun VocaRoute(
                 writtenDate = vocaDetail.writtenDate,
                 isBookmarked = vocaDetail.isBookmarked,
                 onBookmarkClick = { phraseId, isMarked ->
-                    tracker.logGlobalAction(
-                        trigger = TriggerType.CLICK,
-                        action = "bookmark_action",
-                        properties = mapOf(
-                            "phrase_id" to phraseId,
-                            "bookmark_action" to if (isMarked) "add" else "remove",
-                        ),
-                        currentPage = VOCABULARY,
-                    )
                     viewModel.toggleBookmark(phraseId = phraseId, isMarked = isMarked)
                 },
                 isTtsPlaying = ttsState.isPlaying,
