25b96ba04676c074c231661290cc2f00297c8e56
Nahyun Kim <hedghog1004@naver.com>
2026-08-21 04:15:08 +0900

    feat: 4×2 위젯 선택 옵션 추가

--- presentation/widget/src/main/java/com/hilingual/presentation/widget/streak/StreakWidgetReceiver.kt
+++ presentation/widget/src/main/java/com/hilingual/presentation/widget/streak/StreakWidgetReceiver.kt
@@ -3,6 +3,10 @@ package com.hilingual.presentation.widget.streak
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 
-class StreakWidgetReceiver : GlanceAppWidgetReceiver() {
+class StreakSmallWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = StreakWidget()
+}
+
+class StreakLargeWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = StreakWidget()
 }
--- presentation/widget/src/main/java/com/hilingual/presentation/widget/topic/RecommendedTopicWidgetReceiver.kt
+++ presentation/widget/src/main/java/com/hilingual/presentation/widget/topic/RecommendedTopicWidgetReceiver.kt
@@ -3,6 +3,10 @@ package com.hilingual.presentation.widget.topic
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 
-class RecommendedTopicWidgetReceiver : GlanceAppWidgetReceiver() {
+class RecommendedTopicSmallWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = RecommendedTopicWidget()
+}
+
+class RecommendedTopicLargeWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = RecommendedTopicWidget()
 }
